eae46e654da690f19e612ffe2963cc734df57859
941n <googlgain@gmail.com>
2025-06-10 22:44:28 +0900

    Feature/web audio api (#4)
    * fixed: existing reference links in html
    
    * modified: file names for managing existing JavaScript versions
    
    * perf: eliminate latency by replacing Audio() with Web Audio API
    
    * feat: add config overlay with mute instructions and backdrop functionality
    
    * feat: add backdrop and config overlay styles to enhance UI
    
    * fix: ensure audio context resumes on DOMContentLoaded
    
    * fix: add touch-action property to #pads for improved touch handling
    
    * fix: add user-scalable property to viewport meta tag for better responsiveness
    
    * fix: enhance user experience by ensuring consistent styling for interactive elements
    
    * fixed: After checking Chrome for developers, I found that AudioContext object is created in paused state before receiving user action. Change event to 'click' instead of 'DOMContentLoaded' to call resume() after user action.
    
    * fixed: update apple-touch-icon links to use the correct favicon file

---
+++ scripts/01.sampler.js
@@ -0,0 +1,55 @@
+const pads = document.querySelectorAll('.pad');
+
+/** audioCtx() */
+const audioCtx = new AudioContext();
+
+/** samplePaths() */
+const samplePaths = new Map();
+
+/** sampleStates() */
+const sampleStates = new Map();
+
+async function loadSample(samplePath) {
+  const res = await fetch(samplePath);
+  console.log(`Fetching sample from: ${samplePath}`);
+  const arraySample = await res.arrayBuffer();
+  console.log(`Sample fetched, decoding audio data for: ${samplePath}`);
+  return audioCtx.decodeAudioData(arraySample);
+}
+
+document.addEventListener(
+  'click',
+  async () => {
+    await audioCtx.resume();
+  },
+  { once: true }
+);
+
+pads.forEach(pad => {
+  const samplePath = pad.dataset.samplepath;
+
+  if (samplePath) {
+    loadSample(samplePath).then(sample => {
+      samplePaths.set(pad, sample);
+    });
+  }
+
+  pad.addEventListener('click', () => {
+    const currentSample = sampleStates.get(pad);
+
+    if (pad.id === 'pause-pad') {
+      sampleStates.forEach(sample => {
+        sample.stop(0);
+      });
+      sampleStates.clear();
+    } else if (currentSample) {
+      currentSample.stop(0);
+    }
+
+    const sample = audioCtx.createBufferSource();
+    sample.buffer = samplePaths.get(pad);
+    sample.connect(audioCtx.destination);
+    sample.start(0);
+    sampleStates.set(pad, sample);
+  });
+});
